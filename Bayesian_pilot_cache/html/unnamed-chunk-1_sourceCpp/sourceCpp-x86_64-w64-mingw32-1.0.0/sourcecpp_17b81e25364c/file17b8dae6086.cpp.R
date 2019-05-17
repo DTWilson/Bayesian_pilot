9d9fd4e9ecec2d5e15a90288eedaad77c91aedf2
Duncan Wilson <meddwilb@leeds.ac.uk>
2019-05-17 08:39:37 +0100

    Revised paper (RW, JB, DW comments); Rmd to be knited

---
+++ Bayesian_pilot_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.0/sourcecpp_17b81e25364c/file17b8dae6086.cpp.R
@@ -0,0 +1,5 @@
+`.sourceCpp_1_DLLInfo` <- dyn.load('C:/Users/meddwilb/Documents/Projects/MRC SDF/WP2/Bayesian_pilot/Bayesian_pilot_cache/html/unnamed-chunk-1_sourceCpp/sourceCpp-x86_64-w64-mingw32-1.0.0/sourcecpp_17b81e25364c/sourceCpp_3.dll')
+
+timesTwo <- Rcpp:::sourceCppFunction(function(x) {}, FALSE, `.sourceCpp_1_DLLInfo`, 'sourceCpp_1_timesTwo')
+
+rm(`.sourceCpp_1_DLLInfo`)
